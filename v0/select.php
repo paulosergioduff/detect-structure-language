c320d729d386b469467c0e841514c3fdd714f6f4
paulosergioduff <paulosergioduff@gmail.com>
2022-02-24 14:39:11 -0300

    Insercao de estrutura feita corretamente no banco

--- v0/select.php
+++ v0/select.php
@@ -1,15 +1,14 @@
 <?php 
 
 $servername = "localhost";
-$username = "root";
+$targetname = "root";
 $password = "";
 $dbname = "ml_structure";
 
-$getSentence = $_GET['sentence'];
-$familySentence = substr_count($getSentence, " ") + 1;
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $targetname, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -17,16 +16,53 @@ if ($conn->connect_error) {
 
 $mysqli = new mysqli('localhost', 'root', '', 'ml_structure');
 
-$user = 'how are dumb';
-$sql = "SELECT * FROM `snapshot` WHERE `structure` = '{$user}'"; //monto a query
+
+$getSentence = $_GET['sentence'];
+
+
+
+$familySentence = substr_count($getSentence, " ") + 1;
+
+$target = $getSentence;
+$sql = "SELECT * FROM `snapshot` WHERE `structure` = '{$target}'"; //monto a query
 
 
 $query = $mysqli->query( $sql ); //executo a query
 
 if( $query->num_rows > 0 ) {//se retornar algum resultado
-  echo 'Já existe!';
+
+  // Seleciona ID que foi encontrado
+    $sql = "SELECT id, structure, ocorrencies, family FROM snapshot";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+      // output data of each row
+      while($row = mysqli_fetch_assoc($result)) {
+        // Atualiza registro de id encontrado
+            $idEncontrado = $row["id"];
+            $ocorrencies = $row["ocorrencies"] + 1;
+            
+            // Se o id for o mesmo que o alvo encontrado
+            if ($row["structure"] == $target) {
+                $sql = "UPDATE snapshot SET ocorrencies=$ocorrencies WHERE id=$idEncontrado";
+                if ($conn->query($sql) === TRUE) {
+                  echo "Record updated successfully";
+                } else {
+                  echo "Error updating record: " . $conn->error;
+                }
+            }
+        // fim de atualização
+      }
+    }
+  
 } else {
-  echo 'Não existe ainda!';
+        $sql = "INSERT INTO snapshot (structure, ocorrencies, family)
+        VALUES (\"$target\", 1, $familySentence)";
+
+        if ($conn->query($sql) === TRUE) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
 }
 
 $conn->close();
